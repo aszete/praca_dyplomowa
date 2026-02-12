240b27bf49fe5d685fe85ce3205f409f65ec6122
Asia Sz <128564227+aszete@users.noreply.github.com>
2026-02-12 21:32:10 +0100

    Update bronze_quality_check.sql

--- skrypty/silver/bronze_quality_check.sql
+++ skrypty/silver/bronze_quality_check.sql
@@ -14,3 +14,20 @@ WHERE		street != TRIM(street);
 SELECT		postal_code
 FROM		bronze.addresses
 WHERE		city != TRIM(city);
+
+-- customer
+SELECT		first_name
+FROM		bronze.customers
+WHERE		first_name != TRIM(first_name);
+
+SELECT		last_name
+FROM		bronze.customers
+WHERE		last_name != TRIM(last_name);
+
+SELECT		email
+FROM		bronze.customers
+WHERE		email != TRIM(email);
+
+SELECT		gender, COUNT(*)
+FROM		bronze.customers
+GROUP BY	gender;
