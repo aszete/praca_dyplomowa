38d5e5225c48c2ca1977ac3f9d68f2331bb7d8c1
Asia Sz <128564227+aszete@users.noreply.github.com>
2026-02-13 01:04:17 +0100

    Update bronze_quality_check.sql

--- skrypty/silver/bronze_quality_check.sql
+++ skrypty/silver/bronze_quality_check.sql
@@ -1,4 +1,10 @@
 -- address
+-- check for duplicates in IDs
+SELECT		address_id, COUNT(*)
+FROM		bronze.customers
+GROUP BY	address_id
+HAVING count(*) > 1;
+
 SELECT		country
 FROM		bronze.addresses
 WHERE		city != TRIM(city);
@@ -16,6 +22,17 @@ FROM		bronze.addresses
 WHERE		city != TRIM(city);
 
 -- customer
+-- check for duplicates in IDs
+SELECT		customer_id, COUNT(*)
+FROM		bronze.customers
+GROUP BY	customer_id
+HAVING count(*) > 1;
+
+SELECT		address_id, COUNT(*)
+FROM		bronze.customers
+GROUP BY	address_id
+HAVING count(*) > 1;
+
 SELECT		first_name
 FROM		bronze.customers
 WHERE		first_name != TRIM(first_name);
@@ -31,3 +48,33 @@ WHERE		email != TRIM(email);
 SELECT		gender, COUNT(*)
 FROM		bronze.customers
 GROUP BY	gender;
+
+--categories
+-- Check for duplicated in IDs
+SELECT		category_id, COUNT(*)
+FROM		bronze.categories
+GROUP BY	category_id
+HAVING count(*) > 1;
+
+--brands
+-- Check for duplicated in IDs
+SELECT		brand_id, COUNT(*)
+FROM		bronze.brands
+GROUP BY	brand_id
+HAVING count(*) > 1;
+
+SELECT		brand_id, COUNT(*)
+FROM		  bronze.brands
+GROUP BY	brand_id;
+
+--payment methods
+-- Check for duplicated in IDs
+SELECT		payment_method_id, COUNT(*)
+FROM		bronze.payment_methods
+GROUP BY	payment_method_id
+HAVING count(*) > 1;
+
+SELECT		payment_method_id, COUNT(*)
+FROM		  bronze.payment_methods
+GROUP BY	payment_method_id;
+
