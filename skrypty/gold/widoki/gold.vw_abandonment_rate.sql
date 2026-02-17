3c13f7f3f7703f516f847559d3a39b49f98f0c32
Asia Sz <128564227+aszete@users.noreply.github.com>
2026-02-18 00:18:46 +0100

    Create gold.vw_abandonment_rate.sql

---
+++ skrypty/gold/widoki/gold.vw_abandonment_rate.sql
@@ -0,0 +1,14 @@
+-- Widok przedstawiający wskaźnik porzucenia dla poszczególnych stron skepu internetowego
+
+CREATE OR ALTER VIEW gold.vw_abandonment_rate AS
+SELECT 
+    wa.page_name,
+    COUNT(DISTINCT wa.session_key) AS sessions_visited,
+    COUNT(DISTINCT CASE WHEN fs.resulted_in_sale = 1 THEN wa.session_key END) AS sessions_converted,
+    COUNT(DISTINCT CASE WHEN fs.resulted_in_sale = 0 THEN wa.session_key END) AS sessions_abandoned,
+    ROUND(CAST(COUNT(DISTINCT CASE WHEN fs.resulted_in_sale = 0 THEN wa.session_key END) AS FLOAT) / 
+        NULLIF(COUNT(DISTINCT wa.session_key), 0) * 100, 2) AS abandonment_rate
+FROM gold.fact_web_analytics wa
+JOIN gold.fact_sessions fs ON wa.session_key = fs.session_key
+GROUP BY wa.page_name
+ORDER BY abandonment_rate DESC;
